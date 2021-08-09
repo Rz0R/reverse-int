83a421cfbb041ab81a52af4f8c3542af69a086f4
Rz0R <vpa05@mail.ru>
2021-08-09 22:20:21 +1000

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+  num = Math.abs(num);
+  let revNum = 0;
+  while (num > 0) {
+    revNum = revNum * 10 + num % 10;
+    num = Math.floor(num / 10);
+  }
+  return revNum;
 }
